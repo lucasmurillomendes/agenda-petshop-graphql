0bcdb3b48f0a4292917a04499bbb474311e6ab44
Lucas Mendes <lucasmendesifpr@gmail.com>
2020-04-30 17:33:38 -0300

    iniciando projeto

---
+++ infraestrutura/conexao.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql')
+
+const conexao = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: '88334689',
+  database: 'agendaPetshop'
+})
+
+module.exports = conexao
---
+++ server.js
@@ -0,0 +1,34 @@
+const { GraphQLServer } = require ('graphql-yoga')
+const conexao = require('./infraestrutura/conexao')
+const Tabelas = require('./infraestrutura/database/tabelas')
+const Operations = require('./infraestrutura/operations')
+
+conexao.connect(erro => {
+  if (erro) {
+    console.log(erro)
+  }
+
+  console.log('conectou no banco')
+
+  Tabelas.init(conexao)
+})
+ const Clientes = new Operations('cliente')
+ const resolvers = {
+   Query: {
+     status: () => "Servidor Rodando!",
+     clientes: () => Clientes.lista()
+   },
+   Mutation: {
+     adicionarCliente: (root, params) => 
+      Clientes.adiciona(params)
+    
+       }
+
+ }
+const servidor = new GraphQLServer({
+  resolvers,
+  typeDefs: './schema.graphql'
+})
+
+servidor.start(() => console.log('servidor ouvindo'))
+
